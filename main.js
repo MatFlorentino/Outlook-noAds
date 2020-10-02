7cd15cfc6ac0f1c8c3b1ed7e0844377263770346
Mateus Florentino <mateus.florentino@inf.ufrgs.br>
2020-10-02 10:49:57 -0300

    The announcement blocker will only run with a manual click

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const interval = setInterval(() => {
+    const container = document.querySelector("#owaTimePanelBtn_container")
+    if (container) {
+
+        var buttun = document.createElement('buttun')
+        buttun.innerText = "Bloquear by Mateus :)"
+        buttun.className = "_2-4jfRBkDLE1Xuu-op_VA2 o365sx-button"
+        buttun.onclick = function () {
+            block()
+        }
+        container.appendChild(buttun)
+
+    }
+    clearInterval(interval)
+
+}, 5000)
+
+function block() {
     const ad = document.querySelector("._3_hHr3kfEhbNYRFM5YJxH9")
     if (ad) {
 
-        ad.innerHTML="";
+        ad.innerHTML = "";
         ad.className = 'nothing_here'
         ad.style.display = "none"
     }
-    console.log("test")
-}, 10000)
+}
+
